2e5ad3b55a2c6c2703ccf592ff03d8422601eff3
Arjun C S <92919832+Arjun013H@users.noreply.github.com>
2022-11-29 19:03:23 +0530

    more
    js changes

--- assiment/register.js
+++ assiment/register.js
@@ -6,6 +6,8 @@ function NameCheck() {
     for (var i = 0; i < arrLen; i++) {
         if (y.includes(sp[i])) {
             alert("Speacial charater presant in Name please remove it")
+            document.getElementById("Capname").focus()
+            break
         }
     }
 
@@ -18,9 +20,98 @@ function ValidateEmail() {
     var x = document.getElementById("email").value;
     if (!x.match(mailformat)) {
         alert("You have entered an invalid email address!");
-        return false;    }
-    else {
-        alert("You have entered an invalid email address!");
-        return false;
+        document.getElementById("email").focus()
     }
+}
+
+function PhoneNumberCheck() {
+
+    var x = document.getElementById("phonenumber").value;
+    console.log(x)
+    if (!(x.toString().length == 10)) {
+        alert("Not a valid Phone Number")
+        document.getElementById("phonenumber").focus()
+    }
+}
+
+function PasswordCheck() {
+    var Lendoc = document.getElementById("Len")
+    var Updoc = document.getElementById("Up")
+    var Lpdoc = document.getElementById("Lp")
+    var Nudoc = document.getElementById("Nu")
+    var Spdoc = document.getElementById("Sp")
+    var x = document.getElementById("password").value
+    var upper = /[A-Z]/g
+    var lower = /[a-z]/g
+    var number = /[0-9]/g
+    var sp = /[!@#$%^&*()]/g
+    var y = x.toString().length
+
+    if (y > 8) {
+        Lendoc.classList.remove("invalid")
+        Lendoc.classList.add("valid")
+    }
+    else {
+        Lendoc.classList.remove("valid")
+        Lendoc.classList.add("invalid")
+    }
+    
+    if (!!x.match(upper)) {
+        Updoc.classList.remove("invalid")
+        Updoc.classList.add("valid")
+    }
+    else {
+        Updoc.classList.remove("valid")
+        Updoc.classList.add("invalid")
+    }
+    
+    if (!!x.match(upper)) {
+        Updoc.classList.remove("invalid")
+        Updoc.classList.add("valid")
+    }
+    else {
+        Updoc.classList.remove("valid")
+        Updoc.classList.add("invalid")
+    }
+
+    
+    if (!!x.match(lower)) {
+        Lpdoc.classList.remove("invalid")
+        Lpdoc.classList.add("valid")
+    }
+    else {
+        Lpdoc.classList.remove("valid")
+        Lpdoc.classList.add("invalid")
+    }
+
+    
+    if (!!x.match(number)) {
+        Nudoc.classList.remove("invalid")
+        Nudoc.classList.add("valid")
+    }
+    else {
+        Nudoc.classList.remove("valid")
+        Nudoc.classList.add("invalid")
+    }
+
+
+        
+    if (!!x.match(sp)) {
+        Spdoc.classList.remove("invalid")
+        Spdoc.classList.add("valid")
+    }
+    else {
+        Spdoc.classList.remove("valid")
+        Spdoc.classList.add("invalid")
+    }
+
+
+
+
+
+
+
+
+
+
 }
