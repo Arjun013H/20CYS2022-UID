4ea2b6c5d688fc17b1eed706c22c84829ce4bb1a
Arjun C S <92919832+Arjun013H@users.noreply.github.com>
2022-12-07 12:02:31 +0530

    Update register.js

--- assignment/register.js
+++ assignment/register.js
@@ -1,22 +1,14 @@
 function NameCheck() {
     var x = document.getElementById("Capname").value;
-    var y = x.split("")
-    let sp = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
-    var arrLen = sp.length;
-    for (var i = 0; i < arrLen; i++) {
-        if (y.includes(sp[i])) {
+        var nameRegex = /^[a-zA-Z ]+$/;
+        if (!x.match(nameRegex)) {
             alert("Special character present in Name please remove it")
             document.getElementById("Capname").focus()
-            break
         }
     }
 
-
-}
-
-
 function ValidateEmail() {
-    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var mailformat = /^[\w-\.]+@([\w-]+\.)+[\w-]$/g;
     var x = document.getElementById("email").value;
     if (!x.match(mailformat)) {
         alert("You have entered an invalid email address!");
