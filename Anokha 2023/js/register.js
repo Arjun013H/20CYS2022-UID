8692c32362bd5042df12317436c0422160d7d4f1
Arjun013H <csarjun11@gmail.com>
2022-12-28 17:18:41 +0530

    changed and added js and some css

--- Anokha 2023/js/register.js
+++ Anokha 2023/js/register.js
@@ -1,4 +1,29 @@
-myBtn.onclick = () => window.scrollTo({ top: 0, behavior: 'smooth' })
+function scrollIt(){
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+
+}
+myBtn.onclick = () => scrollIt()
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    myBtn.style.display = "block";
+  } else {
+    myBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
+
 
 function NameCheck() {
     var x = document.getElementById("Capname").value;
